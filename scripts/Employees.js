83e56498575567c4ebaaeb8bec8ec6b1d3cd1d4d
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-31 13:46:13 -0500

    Added onClick listener for tracking employee sales totals

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,15 +1,30 @@
-import { getEmployees } from "./database.js";
+import { getEmployees, getOrders } from "./database.js";
 
 const employees = getEmployees();
+const orders = getOrders();
 
 export const Employees = () => {
   let html = "<ul>";
 
   for (const employee of employees) {
-    html += `<li>${employee.name}</li>`;
+    html += `<li data-type="employee" data-id="${employee.id}" data-name="${employee.name}">${employee.name}</li>`;
   }
 
   html += "</ul>";
 
   return html;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  let salesCount = 0;
+  let employeeId = itemClicked.dataset.id;
+  if (itemClicked.dataset.type === "employee") {
+    for (const order of orders) {
+      if (parseInt(employeeId) === order.employeeId) {
+        salesCount++;
+      }
+    }
+    window.alert(`${itemClicked.dataset.name} sold ${salesCount} products`);
+  }
+});
